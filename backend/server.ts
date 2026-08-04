f2b21e5fa4d0a04d5ac32c47cf908217b5a1aa5f
Benjakr04 <benjaminkravchuk04@gmail.com>
2026-08-04 10:54:27 -0300

    feat: setup inicial backend con express, typescript y prisma

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,8 @@
-import app from "./src/app.js";
-import {env} from "./src/config/env.js"
+import app from "./src/app";
+import { env } from "./src/config/env";
 
 const PORT = env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Servidor ejecutándose esn puerto ${PORT}`);
+  console.log(`Servidor ejecutándose en puerto ${PORT}`);
 });
